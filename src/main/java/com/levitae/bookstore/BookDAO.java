2cd60c4cd2cec475d3995118b800b604c395adca
Karina Virgi <karin@Karinas-MacBook-Air.local>
2021-12-01 23:42:11 +0700

    BookDao and Controller

--- src/main/java/com/levitae/bookstore/BookDAO.java
+++ src/main/java/com/levitae/bookstore/BookDAO.java
@@ -1,8 +1,10 @@
 package com.levitae.bookstore;
 
 import com.mysql.jdbc.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * AbstractDAO.java This DAO class provides CRUD database operations for the
@@ -29,4 +31,114 @@ public class BookDAO {
             jdbcConnection = (Connection) DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
         }
     }
+
+    protected void disconnect() throws SQLException {
+        if (jdbcConnection != null && !jdbcConnection.isClosed()) {
+            jdbcConnection.close();
+        }
+    }
+
+    public boolean insertBook(Book book) throws SQLException {
+        String sql = "INSERT INTO book (title, author, price) VALUES (?,?,?)";
+
+        connect();
+
+        PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+        statement.setString(1, book.getTitle());
+        statement.setString(2, book.getAuthor());
+        statement.setFloat(3, book.getPrice());
+
+        boolean rowInserted = statement.executeUpdate() > 0;
+        statement.close();
+        disconnect();
+        return rowInserted;
+    }
+
+    public List<Book> listAllBooks() throws SQLException {
+        List<Book> listBook = new ArrayList<>();
+
+        String sql = "SELECT * FROM book";
+
+        connect();
+
+        Statement statement = jdbcConnection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) {
+            int id = resultSet.getInt("book_id");
+            String title = resultSet.getString("title");
+            String author = resultSet.getString("author");
+            float price = resultSet.getFloat("price");
+
+            Book book = new Book(id, title, author, price);
+            listBook.add(book);
+        }
+
+        resultSet.close();
+        statement.close();
+
+        disconnect();
+
+        return listBook;
+    }
+
+    public boolean deleteBook(Book book) throws SQLException {
+        String sql = "DELETE FROM book where book_id = ?";
+
+        connect();
+
+        PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+        statement.setInt(1, book.getId());
+
+        boolean rowDelete = statement.executeUpdate() > 0;
+        statement.close();
+        disconnect();
+        return rowDelete;
+    }
+
+    public boolean updateBook(Book book) throws SQLException {
+        String sql = "UPDATE book SET title = ?, author = ?, price = ?";
+        sql += " WHERE book_id = ?";
+
+        connect();
+
+        PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+        statement.setString(1, book.getTitle());
+        statement.setString(2, book.getAuthor());
+        statement.setFloat(3, book.getPrice());
+        statement.setInt(4, book.getId());
+
+        boolean rowUpdate = statement.executeUpdate() > 0;
+        statement.close();
+        disconnect();
+        return rowUpdate;
+
+    }
+
+    public Book getBook(int id) throws SQLException {
+        Book book = null;
+        String sql = "SELECT * FROM book where book_id = ?";
+
+        connect();
+
+        PreparedStatement statement = jdbcConnection.prepareStatement(sql);
+        statement.setInt(1, id);
+
+        ResultSet resultSet = statement.executeQuery();
+
+        if (resultSet.next()) {
+            String title = resultSet.getString("title");
+            String author = resultSet.getString("author");
+            float price = resultSet.getFloat("price");
+
+            book = new Book(id, title, author, price);
+        }
+
+        resultSet.close();
+        statement.close();
+
+        return book;
+    }
+
+
 }
